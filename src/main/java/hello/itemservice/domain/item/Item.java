8616ec85c9edc9f864fd120ea3663b5d26089c7a
seonghyeoklee <dltjdgur327@gmail.com>
2021-11-22 23:26:35 +0900

    feat : lombok 어노테이션 수정

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,8 +1,11 @@
 package hello.itemservice.domain.item;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+@Getter @Setter
+@ToString
 public class Item {
 
     private Long id;
